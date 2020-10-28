f3fc88e633fbb83d8f48d286de1cf941e8c16da7
IamPain7 <jjvirtudazo@gmail.com>
2020-10-27 23:14:48 -0600

    even

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -4,9 +4,32 @@ namespace OddOrEven
 {
     class Program
     {
+
+        /*
+     * Step 1:
+    Fork this project: 
+    https://github.com/mvdoyle/OddOrEven
+
+    Step 2: Clone the project to your machine
+    cd repos
+    cd OddOrEven
+    
+
+    Step 3: Open the project in Visual Studio Community 2019
+    Open solution file - OddOrEven.sln
+    
+    Step 4: Go outside the scope of your main method and define your own method that accepts an integer as a parameter and determines if that integer is odd.
+
+
+
+
+     Checklist: Step 1 Done
+               Step 2 DOne
+               Step 3 Done
+     */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
         }
     }
 }
