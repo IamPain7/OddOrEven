041f9a7638a6fc096f8ce961f167745307673123
IamPain7 <jjvirtudazo@gmail.com>
2020-10-27 23:30:19 -0600

    File updat v2

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,40 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Executing
+
+            Console.WriteLine(IsOdd(5));
+
+
+
+
+        }
+
+        //Step 4 Creating own method
+
+        public static bool IsOdd(int num)
+        {
+
+
+            if (num % 2 == 0)
+            {
+
+                Console.WriteLine(" is true");
+                return false;
+
+            }
+
+            else
+            {
+                Console.WriteLine(" is even");
+                return true;
+
+            }
+
+
+
         }
     }
 }
+
+
